385b7439d8cf64924366f9a9ec03c70df879906c
Anti-Ventus <116527814+Anti-Ventus@users.noreply.github.com>
2024-11-01 00:17:08 -0500

    Add files via upload

---
+++ app/src/main/java/edu/utsa/cs3443/msaid/AlarmAddActivity.java
@@ -0,0 +1,39 @@
+package edu.utsa.cs3443.msaid;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AlarmAddActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_add);
+        Intent alarmLink = new Intent(this, AlarmActivity.class);
+        Intent homeLink = new Intent(this, AlarmActivity.class);
+
+
+        Button confirmButton = findViewById(R.id.confirm_button);
+        Button backButton = findViewById(R.id.back_button);
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Alarm added", Toast.LENGTH_LONG).show();
+                startActivity(alarmLink);
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Moving back to Alarms", Toast.LENGTH_LONG).show();
+                startActivity(homeLink);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/edu/utsa/cs3443/msaid/AlarmDeleteActivity.java
@@ -0,0 +1,40 @@
+package edu.utsa.cs3443.msaid;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AlarmDeleteActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_delete);
+        Intent confirmLink = new Intent(this, AlarmConfirmationActivity.class);
+        Intent alarmLink = new Intent(this, AlarmActivity.class);
+
+
+        Button confirmButton = findViewById(R.id.confirm_button);
+        Button backButton = findViewById(R.id.back_button);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Moving to Confirmation", Toast.LENGTH_LONG).show();
+                startActivity(confirmLink);
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Moving back to Alarms", Toast.LENGTH_LONG).show();
+                startActivity(alarmLink);
+            }
+        });
+
+    }
+}
