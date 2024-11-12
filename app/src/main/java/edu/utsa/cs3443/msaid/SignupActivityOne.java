e1f4f5b19409666441cda247e69cacaa4fd7b15d
Trey <trey.lynn.pickens@gmail.com>
2024-11-12 08:47:55 -0600

    Version -1.4.0

--- app/src/main/java/edu/utsa/cs3443/msaid/SignupActivityOne.java
+++ app/src/main/java/edu/utsa/cs3443/msaid/SignupActivityOne.java
@@ -4,26 +4,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivityOne extends AppCompatActivity {
-    /*
-        TODO: Get items from TextView xml objects and add them to next Intent
-     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_one);
         Intent continueSignup = new Intent(this, SignupActivityTwo.class);
-
         Button signupButton = findViewById(R.id.complete_signup);
+        EditText usernameInput = findViewById(R.id.username_input);
+        EditText passwordInput = findViewById(R.id.password_input);
+        EditText confirmInput = findViewById(R.id.confirm_password_input);
+
+
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(view.getContext(), "Sign Up Pressed", Toast.LENGTH_LONG).show();
-
+                String username = usernameInput.getText().toString();
+                String password = passwordInput.getText().toString();
+                String confirmPassword = confirmInput.getText().toString();
+                if(username.isEmpty()){
+                    Toast.makeText(view.getContext(), "Username field cannot be blank.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(password.isEmpty()){
+                    Toast.makeText(view.getContext(), "Password field cannot be blank.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(!(password.equals(confirmPassword))){
+                    Toast.makeText(view.getContext(), "Passwords must match.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                continueSignup.putExtra("username", username);
+                continueSignup.putExtra("password", password);
                 startActivity(continueSignup);
             }
         });
